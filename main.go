2eb56bcbc0f853fe796196bab22b706521e44d30
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-06-12 23:12:32 +0900

    マークダウン化処理追加、コード整理

--- main.go
+++ main.go
@@ -2,35 +2,11 @@ package main
 
 import (
 	"fmt"
+	"regexp"
+	"strings"
 
 	"github.com/gocolly/colly/v2"
 )
-
-// func main() {
-// 	// デフォルトのコレクターを作成
-// 	c := colly.NewCollector(
-// 		// 訪問するドメインを hackerspaces.org, wiki.hackerspaces.org のみに制限
-// 		colly.AllowedDomains("hackerspaces.org", "wiki.hackerspaces.org"),
-// 	)
-
-// 	// href属性を持つa要素ごとにコールバックを実行
-// 	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-// 		link := e.Attr("href")
-// 		// リンクを表示
-// 		fmt.Printf("リンク発見: %q -> %s\n", e.Text, link)
-// 		// ページ内で見つかったリンクを訪問
-// 		// AllowedDomains に含まれるリンクのみ訪問される
-// 		c.Visit(e.Request.AbsoluteURL(link))
-// 	})
-
-// 	// リクエスト前に "Visiting ..." を表示
-// 	c.OnRequest(func(r *colly.Request) {
-// 		fmt.Println("訪問中", r.URL.String())
-// 	})
-
-// 	// https://hackerspaces.org からスクレイピングを開始
-// 	c.Visit("https://nextjs-root-5.vercel.app/")
-// }
 
 func main() {
 	// デフォルトのコレクターを作成
@@ -39,6 +15,19 @@ func main() {
 		colly.AllowedDomains("nextjs-root-5.vercel.app"),
 		colly.MaxDepth(1),
 	)
+
+	// href属性を持つa要素ごとにコールバックを実行
+	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
+		link := e.Attr("href")
+		// リンクを表示
+		fmt.Println("    - ", link)
+
+		// ページ内で見つかったリンクを訪問
+		// e.Request.Visit(link)
+
+		// AllowedDomains に含まれるリンクのみ訪問される
+		// c.Visit(e.Request.AbsoluteURL(link))
+	})
 
 	// テキストコンテンツを抽出するためのコールバック
 	c.OnHTML("html", func(e *colly.HTMLElement) {
@@ -54,50 +43,79 @@ func main() {
 			description = "--"
 		}
 
-		// ルートパス以外では header, footer, script タグを削除
-		if e.Request.URL.Path != "/" {
-			e.DOM.Find("header").Remove()
-			e.DOM.Find("footer").Remove()
-		}
-
-		// head タグと script タグは常時削除
+		// head, header, footer, script タグを削除
 		e.DOM.Find("head").Remove()
+		e.DOM.Find("header").Remove()
+		e.DOM.Find("footer").Remove()
 		e.DOM.Find("script").Remove()
 
-		// テキストコンテンツをマークダウン形式に変換しながら取得
-		textContent := e.DOM.Text()
-		// markdown = markdownify(textContent)
+		// HTMLをマークダウン形式に変換して取得
+		html, err := e.DOM.Html()
+		if err != nil {
+			fmt.Println("HTMLの取得に失敗しました:", err)
+			return
+		}
+		markdown := convertHtmlToMarkdown(html)
 
-		fmt.Println("ページタイトル:", pageTitle)
-		fmt.Println("")
-		fmt.Println("ページディスクリプション:", description)
-		fmt.Println("")
-		fmt.Println("ページテキスト:", textContent)
-		fmt.Println("")
-		// fmt.Println("ページマークダウン:", markdown)
-		// fmt.Println("")
-	})
-
-	// href属性を持つa要素ごとにコールバックを実行
-	c.OnHTML("a[href]", func(e *colly.HTMLElement) {
-		link := e.Attr("href")
-		// リンクを表示
-		fmt.Println("- リンク発見：", link)
-
-		// ページ内で見つかったリンクを訪問
-		// e.Request.Visit(link)
-
-		// AllowedDomains に含まれるリンクのみ訪問される
-		// c.Visit(e.Request.AbsoluteURL(link))
+		fmt.Println(">> ページタイトル:", pageTitle)
+		fmt.Println(">> ページディスクリプション:", description)
+		fmt.Println(">> メイン部のマークダウン:\n", markdown)
+		fmt.Println("\n\n---\n\n")
 	})
 
 	// リクエスト前に "アクセス >> " を表示
 	c.OnRequest(func(r *colly.Request) {
-		fmt.Println("アクセス >> ", r.URL.String())
-		fmt.Println("")
-		fmt.Println("")
+		fmt.Println(">> アクセス中... ", r.URL.String())
+		fmt.Println(">> リンク取得:")
 	})
 
 	// https://nextjs-root-5.vercel.app/ からスクレイピングを開始
 	c.Visit("https://nextjs-root-5.vercel.app/")
+}
+
+// HTML をマークダウン形式に変換する関数、対応するのは h1~h6, ul, ol, li, table, tr, td, th, p タグ
+func convertHtmlToMarkdown(html string) string {
+	var markdown strings.Builder
+
+	// タグとマークダウンの対応表
+	replacer := []struct {
+		reOpen  *regexp.Regexp
+		reClose *regexp.Regexp
+		mdOpen  string
+		mdClose string
+	}{
+		{regexp.MustCompile(`(?i)<h1[^>]*>`), regexp.MustCompile(`(?i)</h1>`), "# ", "\n\n"},
+		{regexp.MustCompile(`(?i)<h2[^>]*>`), regexp.MustCompile(`(?i)</h2>`), "## ", "\n\n"},
+		{regexp.MustCompile(`(?i)<h3[^>]*>`), regexp.MustCompile(`(?i)</h3>`), "### ", "\n\n"},
+		{regexp.MustCompile(`(?i)<h4[^>]*>`), regexp.MustCompile(`(?i)</h4>`), "#### ", "\n\n"},
+		{regexp.MustCompile(`(?i)<h5[^>]*>`), regexp.MustCompile(`(?i)</h5>`), "##### ", "\n\n"},
+		{regexp.MustCompile(`(?i)<h6[^>]*>`), regexp.MustCompile(`(?i)</h6>`), "###### ", "\n\n"},
+		{regexp.MustCompile(`(?i)<ul[^>]*>`), regexp.MustCompile(`(?i)</ul>`), "", "\n"},
+		{regexp.MustCompile(`(?i)<ol[^>]*>`), regexp.MustCompile(`(?i)</ol>`), "", "\n"},
+		{regexp.MustCompile(`(?i)<li[^>]*>`), regexp.MustCompile(`(?i)</li>`), "- ", "\n"},
+		{regexp.MustCompile(`(?i)<table[^>]*>`), regexp.MustCompile(`(?i)</table>`), "", "\n\n"},
+		{regexp.MustCompile(`(?i)<tr[^>]*>`), regexp.MustCompile(`(?i)</tr>`), "", "\n"},
+		{regexp.MustCompile(`(?i)<th[^>]*>`), regexp.MustCompile(`(?i)</th>`), "| ", " "},
+		{regexp.MustCompile(`(?i)<td[^>]*>`), regexp.MustCompile(`(?i)</td>`), "| ", " "},
+		{regexp.MustCompile(`(?i)<p[^>]*>`), regexp.MustCompile(`(?i)</p>`), "", "\n\n"},
+		{regexp.MustCompile(`(?i)<div[^>]*>`), regexp.MustCompile(`(?i)</div>`), "", "\n\n"},
+	}
+
+	md := html
+	for _, r := range replacer {
+		md = r.reOpen.ReplaceAllString(md, r.mdOpen)
+		md = r.reClose.ReplaceAllString(md, r.mdClose)
+	}
+
+	// 残ったタグをすべて除去
+	reTag := regexp.MustCompile(`(?s)<[^>]+>`)
+	md = reTag.ReplaceAllString(md, "")
+
+	// 改行を適切に処理
+	md = strings.TrimSpace(md)
+	// 連続する改行を1つにまとめる
+	md = regexp.MustCompile(`\n{2,}`).ReplaceAllString(md, "\n\n")
+
+	markdown.WriteString(md)
+	return markdown.String()
 }
