f2aae437426eb434975b85d150106a4c802dff95
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-08-12 01:07:04 +0900

    コード整理

--- nlp/api/api.go
+++ nlp/api/api.go
@@ -10,7 +10,7 @@ import (
 )
 
 // ====================================================================================
-// API関数
+// 定数と構造体定義
 // ====================================================================================
 
 var port = "8000"
@@ -26,6 +26,10 @@ type ConvertResponse struct {
 	Vector []float32 `json:"vector"`
 }
 
+// ====================================================================================
+// API関数
+// ====================================================================================
+
 // APIサーバーを起動する関数
 func StartServer() {
 	http.HandleFunc("/", handler)
@@ -36,24 +40,10 @@ func StartServer() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	// リクエストのメソッドによって処理を分岐
 	switch r.Method {
-	case "GET":
-		getHandler(w, r)
 	case "POST":
 		postHandler(w, r)
 	default:
 		fmt.Fprintf(w, "Method not allowed")
-	}
-}
-
-// GETリクエストを処理する関数
-func getHandler(w http.ResponseWriter, r *http.Request) {
-	// リクエストパスを取得
-	path := r.URL.Path
-
-	// リクエストパスによって処理を分岐
-	switch path {
-	default:
-		fmt.Fprintf(w, "Not found")
 	}
 }
 
@@ -65,55 +55,29 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	// リクエストパスによって処理を分岐
 	switch path {
 	case "/convert":
+		var req ConvertRequest
+
 		// リクエストボディを構造体に変換
-		var request ConvertRequest
-		if err := decodeRequestBody(r, &request); err != nil {
-			http.Error(w, fmt.Sprintf("リクエストボディの解析に失敗: %v", err), http.StatusBadRequest)
+		if err := json.NewDecoder(r.Body).Decode(&req); err != nil {
+			http.Error(w, "無効なリクエストボディ", http.StatusBadRequest)
 			return
 		}
-		// ConvertToVector関数を呼び出してベクトルに変換
-		vector, err := vectorize.TextToVector(request.Text, request.IsQuery)
+
+		// テキストをベクトル化
+		vector, err := vectorize.TextToVector(req.Text, req.IsQuery)
 		if err != nil {
-			http.Error(w, fmt.Sprintf("ベクトル変換に失敗: %v", err), http.StatusInternalServerError)
+			http.Error(w, fmt.Sprintf("ベクトル化エラー: %v", err), http.StatusInternalServerError)
 			return
 		}
-		// レスポンス用の構造体を作成
-		response := ConvertResponse{
-			Vector: vector,
-		}
-		sendJsonResponse(w, response)
+
+		// レスポンスを構造体に変換
+		response := ConvertResponse{Vector: vector}
+
+		// レスポンスをJSON形式で返す
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(response)
 
 	default:
 		fmt.Fprintf(w, "Not found")
 	}
-}
-
-// ====================================================================================
-// リクエストボディの処理関数
-// ====================================================================================
-// リクエストボディを構造体に変換する関数
-func decodeRequestBody(r *http.Request, v interface{}) error {
-	// リクエストボディを直接JSONとして読み込む
-	decoder := json.NewDecoder(r.Body)
-	if err := decoder.Decode(v); err != nil {
-		return err
-	}
-	defer r.Body.Close()
-	return nil
-}
-
-// ====================================================================================
-// レスポンスの処理関数
-// ====================================================================================
-// 構造体をjson形式の文字列に変換してレスポンスを返す関数
-func sendJsonResponse(w http.ResponseWriter, data interface{}) {
-	jsonBytes, err := json.Marshal(data)
-	if err != nil {
-		fmt.Fprintf(w, "JSON変換に失敗: %v", err)
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-		return
-	}
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(jsonBytes)
 }
