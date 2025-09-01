7eb74a24e5bf5500791e8c89a07e72165e78d1fc
root-5 <hujisannrokuoumunaku.shigoto@gmail.com>
2025-09-02 01:58:26 +0900

    モデルたたき台作成とオーバーラップ変更

--- app/domain/model/model.go
+++ app/domain/model/model.go
@@ -23,38 +23,51 @@ type Page struct {
 	Keywords    string    `bun:"keywords,notnull,type:varchar(255)" json:"keywords"`                    // キーワード
 	Markdown    string    `bun:"markdown,notnull,type:text" json:"markdown"`                            // Markdown コンテンツ
 	Hash        string    `bun:"hash,notnull,type:char(64)" json:"-"`                                   // コンテンツのハッシュ値
-	Vector      []float32 `bun:"vector,notnull,type:vector(384)" json:"-"`                              // ベクトルデータ（モデルの次元数に合わせて変更）
 	CreatedAt   time.Time `bun:",notnull,default:current_timestamp,type:timestamptz" json:"-"`          // 作成日時
 	UpdatedAt   time.Time `bun:",notnull,default:current_timestamp,type:timestamptz" json:"updated_at"` // 更新日時
 	DeletedAt   time.Time `bun:",soft_delete,type:timestamptz" json:"-"`                                // 削除日時
 }
 
-// 見出し情報
-// type Heading struct {
-// 	bun.BaseModel `bun:"table:headings"`
+// チャンク情報
+type Chunk struct {
+	bun.BaseModel `bun:"table:chunks"`
 
-// 	Id           int64     `bun:"id,pk,autoincrement"`                                 // ID
-// 	PageId       int64     `bun:"page_id,notnull,type:int"`                            // ページID
-// 	HeadingIndex int64     `bun:"heading_index,notnull,type:int"`                      // 見出しインデックス
-// 	HeadingPath  string    `bun:"heading_path,notnull,type:varchar(255)"`              // 見出しパス (title > h1 > h2 > h3 ...)
-// 	CreatedAt    time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 作成日時
-// 	UpdatedAt    time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 更新日時
-// 	DeletedAt    time.Time `bun:",soft_delete,type:timestamptz"`                       // 削除日時
-// }
+	Id          int64     `bun:"id,pk,autoincrement"`                                 // ID
+	PageId      int64     `bun:"page_id,notnull,type:int"`                            // ページID
+	NLPConfigId int64     `bun:"nlp_config_id,notnull,type:int"`                      // NLP設定ID
+	Chunk       string    `bun:"chunk,notnull,type:text"`                             // チャンク
+	CreatedAt   time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 作成日時
+	UpdatedAt   time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 更新日時
+	DeletedAt   time.Time `bun:",soft_delete,type:timestamptz"`                       // 削除日時
+}
 
 // ベクトル情報
 // 一つの対象に複数モデルによって複数ベクトルが作られることが想定されるため、ベクトルのテーブルは独立しているべき
-// type Embedding struct {
-// 	bun.BaseModel `bun:"table:embeddings"`
+type Vector struct {
+	bun.BaseModel `bun:"table:vectors"`
 
-// 	Id        int64     `bun:"id,pk,autoincrement"`                                 // ID
-// 	ChunkId   int64     `bun:"chunk_id,notnull,type:int"`                           // チャンクID
-// 	Vector    []float32 `bun:"vector,notnull,type:vector(1536)"`                    // ベクトルデータ
-// 	ModelName string    `bun:"model_name,notnull,type:varchar(100)"`                // モデル名
-// 	CreatedAt time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 作成日時
-// 	UpdatedAt time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 更新日時
-// 	DeletedAt time.Time `bun:",soft_delete,type:timestamptz"`                       // 削除日時
-// }
+	Id          int64     `bun:"id,pk,autoincrement"`                                 // ID
+	ChunkId     int64     `bun:"chunk_id,notnull,type:int"`                           // チャンクID
+	NLPConfigId int64     `bun:"nlp_config_id,notnull,type:int"`                      // NLP設定ID
+	Vector      []float32 `bun:"vector,notnull,type:vector(384)"`                     // ベクトルデータ（モデルの次元数に合わせて変更）
+	CreatedAt   time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 作成日時
+	UpdatedAt   time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 更新日時
+	DeletedAt   time.Time `bun:",soft_delete,type:timestamptz"`                       // 削除日時
+}
+
+// NLP設定情報
+type NLPConfig struct {
+	bun.BaseModel `bun:"table:nlp_configs"`
+
+	Id                 int64     `bun:"id,pk,autoincrement"`                                 // ID
+	MaxTokenLength     int64     `bun:"max_token_length,notnull,type:int"`                   // 最大トークン長
+	OverlapTokenLength int64     `bun:"overlap_token_length,notnull,type:int"`               // オーバーラップトークン長
+	ModelName          string    `bun:"model_name,notnull,type:varchar(100)"`                // モデル名
+	ModelVectorLength  int64     `bun:"model_vector_length,notnull,type:int"`                // モデルのベクトル長
+	CreatedAt          time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 作成日時
+	UpdatedAt          time.Time `bun:",notnull,default:current_timestamp,type:timestamptz"` // 更新日時
+	DeletedAt          time.Time `bun:",soft_delete,type:timestamptz"`                       // 削除日時
+}
 
 // 検索履歴情報
 // type SearchLog struct {
